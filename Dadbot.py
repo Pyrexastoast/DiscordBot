27bb328e946ff056e7308371c4e19c851c7cd1e5
Alex Miranker <miranker@stolaf.edu>
2017-06-30 21:08:52 -0400

    upgrades to Dadbot. Added dadjoke feature

--- Dadbot.py
+++ Dadbot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from collections import deque
 import random
 import sys
 import asyncio
@@ -8,6 +9,16 @@ description = '''A discord bot made and implemented (so far) by Alex Miranker. T
 
 bot = commands.Bot(command_prefix='!', description=description)
 
+#Only takes Message.author as argument. Will return
+#   their nickname on that server if they have one.
+#   If not, it will return their username.
+def handle_nick(person):
+    display_name = '{0}'.format(person.nick) if person.nick else '{0}'.format(person.name)
+    return display_name
+
+#Returns a random emoji that is guaranteed to be supported
+#   across discords different apps and can be used as a reaction.
+#   (probably)
 def random_emoji():
     r = random.randint(1,1786)
     try:
@@ -27,18 +38,34 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    for serv in bot.servers:
+        print("Now messaging:\t", serv.name)
+        await bot.send_message(serv.default_channel, "Hello, World!")
 
 @bot.listen()
 async def on_message(message):
-    #we do not want the bot to reply to itself
+    #Prevents the bot from replying to itself
     if message.author == bot.user:
         return
-
-    if message.content.startswith('!Hello'):
-        var = '{0.author.nick}'.format(message) if message.author.nick else '{0.author.name}'.format(message)
-        msg = 'Hello {0.author.mention}'.format(message)
-        await bot.send_message(message.channel, msg)
-
+    
+    #Looks for posts that start with "I am" or "I'm" and flips them around
+    #   For example:
+    #   If user posts: "I'm hungry"
+    #   Bot will post: "Nice to meet you Hungry, I'm Dadbot"
+    #   This needs neatening into one if statement.
+    if message.content.startswith('I am ') or message.content.startswith('i am '):
+        obj = message.content[5:]
+        dadjk = 'Nice to meet you, {0}. I\'m Dadbot'.format(obj.title())
+        await bot.send_message(message.channel, dadjk)
+    if message.content.startswith('I\'m ') or message.content.startswith('i\'m '):
+        obj = message.content[4:]
+        dadjk = 'Nice to meet you, {0}. I\'m Dadbot'.format(obj.title())
+        await bot.send_message(message.channel, dadjk)
+    if message.content.startswith('Im ') or message.content.startswith('im '):
+        obj = message.content[3:]
+        dadjk = 'Nice to meet you, {0}. I\'m Dadbot'.format(obj.title())
+        await bot.send_message(message.channel, dadjk)
+    
 @bot.command(pass_context=True, hidden=True)
 async def clean(ctx):
     """Deletes the previous 100 bot messages
@@ -97,69 +124,62 @@ async def choose(*choices : str):
     await bot.say(random.choice(choices))
 
 @bot.command(pass_context=True)
-async def msginfo(ctx):
-    m1=bot.messages[-1]
-    m2=bot.messages[-2]
-    m3=bot.messages[-3]
-    print('bot.messages[-1] content: {0}'.format(m1.content))
-    print('bot.messages[-2] content: {0}'.format(m2.content))
-    print('bot.messages[-3] content: {0}'.format(m3.content))
-
-@bot.command(pass_context=True)
-async def echo(ctx, txt:str=''):
-    a = chr(0x1F487)
-    b = chr(0x0200D)
-    c = chr(0x02642)
-    d = chr(0x0FE0F)
-     
-    await bot.say(a)
-    await bot.say(''.join((a,b)))
-    await bot.say(''.join((a,b,c)))
-    await bot.say(''.join((a,b,c,d)))
-
-@bot.command(pass_context=True)
-async def rreact(ctx):
-    target = bot.messages[-2]
-    await bot.delete_message(bot.messages[-1])
-    await bot.add_reaction(target, random_emoji())
-    await bot.add_reaction(target, random_emoji())
-
-@bot.command(pass_context=True)
-async def specreact(ctx, emoj:str):
-    target = bot.messages[-2]
-    await bot.delete_message(bot.messages[-1])
-    await bot.add_reaction(target, emoj)
-
-@bot.command()
-async def remoji():
-    rem = list()
-    for i in range(5):
-        rem.append(random_emoji())
-    await bot.say(rem)
-
-@bot.command(pass_context=True)
 async def poll(ctx, *polargs : str):
     """Makes a poll in the current chat.
 
     Enter your question, followed by the different choices.
     Separate the question and all the options with commas.
+
+    If you enter no choices, Dadbot will default to a scheduling
+    poll and use pre-formatted days of the week.
     """
-    asker = '{0.author.nick}'.format(ctx.message) if ctx.message.author.nick else '{0.author.name}'.format(ctx.message)
-    tmp = ' '.join(polargs)
-    arg = list(map(lambda s: s.strip(" "), tmp.split(",")))
-    title = arg.pop(0)
-    opts = list(arg[0:])
+    
+    #Uncommenting this line will tell the bot to delete the 
+    #   message that invokes this command
+    #await bot.delete_message(ctx.message)      
+    
+    #Creates 'asker' using handle_nick(). It is the author's nickname 
+    #   if they have one. Otherwise, 'asker' is set to their name.
+    asker = handle_nick(ctx.message.author)
+    arg = deque(map(lambda s: s.strip(" "), ' '.join(polargs).split(",")))  #Separate the argument by commas
+    title = arg.popleft()           #Parse the question from the options
+    
+    #This segment handles a default case if no options are given.
+    #   in this scenario, week is set to True and the default
+    #   days of the week are used as options for the poll
+    week = False if arg else True    
+    day = ctx.message.timestamp.strftime('%A')      #Get the day of the week from the message timestamp
+    if week:        #Executes if no options are given. Uses default week day options 
+        opts = deque(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+        while day!=opts[-1]:    #Cycle the order until the current day is last
+            opts.rotate(1)               
+        opts[0] = 'Tomorrow'    #Change the first day to 'Tomorrow'
+    else:           #Executes if options are given. Uses given options instead of defaults
+        opts = deque(arg)
+    
+    #Create a list of random emojis for every options
     randemoj = []
     for a in opts:
         randemoj.append(random_emoji())
-    print(randemoj) 
-    #msg = 'Hey , {0} has made a new poll\n\n{1}'.format(asker, title)
+     
+    #Creates the message that the bot will post to the channel. The header
+    #   mentions everyone and then asks the given question. Then each option 
+    #   is formatted and appended to the message after a new line. As a tool 
+    #   for developing, the commented line will remove the @everyone mention 
+    #   
     msg = 'Hey @everyone, {0} has made a new poll\n\n{1}'.format(asker, title)
-    print(opts)
+    #msg = 'Hey , {0} has made a new poll\n\n{1}'.format(asker, title)
     for i,choice in enumerate(opts):
        msg = '\n'.join((msg, '{0}: {1}'.format(randemoj[i], choice)))
     
+    #Sends the message to the chat. The bot.say() method returns the 
+    #   message object that the bot published. Assigning it to a variable 
+    #   here and referencing it in the line below prevents the bot from 
+    #   adding a reaction to the wrong message.
     m = await bot.say(msg) 
+    
+    #Adds each of the random emojis chosen above as a reaction to the poll
+    #   so that people don't have to find the specific emojis themselves
     for e in randemoj:
         await bot.add_reaction(m, e)
     
